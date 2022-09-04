8ec632add652919a89429c939568a25254c9bfe0
ooligors <ooligors@gmail.com>
2022-09-04 19:12:11 +0300

    fixing

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import './index.css';
 import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <BrowserRouter basename="/goit-react-hw-05-movies/">
-<BrowserRouter>
+  
+<BrowserRouter basename="/goit-react-hw-05-movies/">
   <React.StrictMode>
     <App />
   </React.StrictMode>
